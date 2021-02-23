7bec53a324984d51bbf7fcb23f064a8b2e859982
Svetlana Akimova <lana@Svetlanas-MacBook-Pro.local>
2021-02-22 18:43:11 -0800

    just for case do not lost any updates

--- getData.py
+++ getData.py
@@ -8,15 +8,22 @@ engine = create_engine(db_path)
 # conn = engine.connect()
 
 def getCoordinates(state):
+    coords = []
     with engine.connect() as conn:
-        query = f"SELECT geometry FROM resorts_info WHERE state = '{state}'"
-        result = conn.execution_options(stream_results=True).execute(query).fetchall()
+        query = f"SELECT CAST (lat as FLOAT) FROM resorts_info WHERE state = '{state}'"
+        lat = conn.execution_options(stream_results=True).execute(query).fetchall()
 
-        coordinates = []
-        for i in range(len(result)):
-            temp = type(result[i])
+        query = f"SELECT CAST (lon as FLOAT) FROM resorts_info WHERE state = '{state}'"
+        lon = conn.execution_options(stream_results=True).execute(query).fetchall()
 
-    return temp
+        if len(lat) == len(lon):
+            for i in range(len(lat)):
+                print(i, lat[i], lon[i])
+                coords.append([lat[i], lon[i]])
+
+
+
+    return coords
     
 
 print(getCoordinates('California'))
