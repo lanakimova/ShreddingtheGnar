23acaab9dec510e656dfe497b7f9aefd7267e2f6
Svetlana Akimova <lana@Svetlanas-MacBook-Pro.local>
2021-02-23 18:30:30 -0800

    fix type for price and slope len

--- app.py
+++ app.py
@@ -19,19 +19,41 @@ def home():
     return render_template("index.html", states=getStates())
 
 # Route to DB
-@app.route("/available_states")
+@app.route("/getAvailableStates")
 def getStates():
     with engine.connect() as conn:
-        query = f"SELECT state FROM resorts_info WHERE state NOT IN ('Empty', 'Unknown')"
-        states = conn.execution_options(stream_results=True).execute(query).fetchall()
+        query = f"SELECT state FROM resorts_info WHERE state IS NOT NULL GROUP BY state ORDER BY state ASC"
+        tempStates = conn.execution_options(stream_results=True).execute(query).fetchall()
+        states = []
+        for state in tempStates:
+            state = str(state)
+            states.append(state[2:len(state)-3])
     return states
 
+@app.route("/getResortInfo")
+def getResortInfo(resortName):
+    with engine.connect() as conn:
+        query = f"SELECT price, closest_town, total_len, easy_len, intermediate_len, difficult_len, website \
+                FROM resorts_info WHERE name = '{resortName}'"
+        resort_info = conn.execution_options(stream_results=True).execute(query).fetchall()
+    return resort_info
+
+@app.route("/getCoordinates")
+def getCoordinates(resortName):
+    with engine.connect() as conn:
+        query = f"SELECT lat, lon FROM resorts_info WHERE name = '{resortName}'"
+        coordinates = conn.execution_options(stream_results=True).execute(query).fetchall()
+    return list(coordinates[0])
+
+
+# 'Mammoth Mountain'
 # EL - All price, price range -- same function -- should return the price that was requested, default = all
-# LA - Functions that return information about the resorts -- all the info that Ryan scraped for us -- length, price, etc
+# LA (done) - Functions that return information about the resorts -- all the info that Ryan scraped for us -- length, price, etc
     # Do this just for 1 resort, shouldn't return info about all of the resorts at once
 # EL - Add weather.csv to db
     # Jupyter notebook that sets up all of our data-- in the notebook, edit a few rows to add weather data to the resorts database
 # LA - Resorts function that returns resorts name that was chosen for different parameters
+# LA (done) - coordinates
 
 # Route to scrape website
 # @app.route("/scrape")
