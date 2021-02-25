6dc889a121ea179be611005241fea9d90cbbda74
Svetlana Akimova <lana@Svetlanas-MacBook-Pro.local>
2021-02-24 19:48:16 -0800

    maps experiments

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, redirect, jsonify
+from flask.json import tojson_filter
 from sqlalchemy import create_engine
+import json, psycopg2
 # from flask_pymongo import PyMongo
 from config import postgrepass
 import sys
@@ -16,7 +18,7 @@ engine = create_engine(db_path)
 def home():
     # new_resort_info = conn.db.resorts.find_one() # Add the name of the dictionary created with the resort info 
     # print(new_resort_info)
-    return render_template("index.html", states=getStates())
+    return render_template("test-index.html", states=getStates())
 
 # Route to DB
 @app.route("/getAvailableStates")
@@ -28,23 +30,56 @@ def getStates():
         for state in tempStates:
             state = str(state)
             states.append(state[2:len(state)-3])
+        # states = json.dumps(states)
     return states
 
-@app.route("/getResortInfo")
-def getResortInfo(resortName):
+@app.route("/_getResortInfo/")
+def getResortInfo():
+    resortInfo = {}
     with engine.connect() as conn:
+        # query = f"SELECT price FROM resorts_info WHERE name = '{resortName}'"
+        # price = conn.execution_options(stream_results=True).execute(query).fetchall()
+        # price = str(price)
+        # price = price[2:len(price)-3]
+        # resortInfo['price'] = float(price)
+
+        # query = f"SELECT closest_town FROM resorts_info WHERE name = '{resortName}'"
+        # closest_town = conn.execution_options(stream_results=True).execute(query).fetchall()
+        # closest_town = str(closest_town)
+        # closest_town = closest_town[3:len(closest_town)-4]
+        # resortInfo['Closest town'] = closest_town
+
         query = f"SELECT price, closest_town, total_len, easy_len, intermediate_len, difficult_len, website \
-                FROM resorts_info WHERE name = '{resortName}'"
+                FROM resorts_info"
+                # WHERE name = '{resortName}'"
         resort_info = conn.execution_options(stream_results=True).execute(query).fetchall()
     return resort_info
+print(getResortInfo())
 
-@app.route("/getCoordinates")
+@app.route("/getCoordinates/<resortName>")
 def getCoordinates(resortName):
-    with engine.connect() as conn:
-        query = f"SELECT lat, lon FROM resorts_info WHERE name = '{resortName}'"
-        coordinates = conn.execution_options(stream_results=True).execute(query).fetchall()
-    return list(coordinates[0])
 
+    with engine.connect() as conn:
+        geo = {}
+        query = f"SELECT lat, lon  FROM resorts_info WHERE name = '{resortName}'"
+        coordinates = conn.execution_options(stream_results=True).execute(query).fetchall()
+        coordinates = list(coordinates[0])
+        geo['lon'] = coordinates[1]
+        geo['lat'] = coordinates[0]
+    return geo
+    # jsonify([coordinates[1], coordinates[0]])
+    # geo
+
+@app.route("/getResortsNameByState/<state>")
+def getResortsNameByState(state):
+    resortNames = []
+    with engine.connect() as conn:
+        query = f"SELECT name FROM resorts_info WHERE state = '{state}'"
+        names = conn.execution_options(stream_results=True).execute(query).fetchall()
+        for name in names:
+            name = str(name)
+            resortNames.append(name[2:len(name)-3])
+    return jsonify(resortNames)
 
 # 'Mammoth Mountain'
 # EL - All price, price range -- same function -- should return the price that was requested, default = all
