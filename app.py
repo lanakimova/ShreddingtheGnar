c7dc8dfac3c7fe5c8fff68c18b366e196057f217
Svetlana Akimova <havroshik+git@gmail.com>
2021-06-21 11:47:02 -0700

    clean repo

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 from flask import Flask, render_template, redirect, jsonify
-from flask.json import tojson_filter
 
 import csv
-# from sqlalchemy import create_engine
-# import json, psycopg2
-# # from flask_pymongo import PyMongo
-# from config import postgrepass
-# import sys
 
 app = Flask(__name__)
 
-# Establish Mongo connection with PyMongo
-# conn = PyMongo(app, uri="mongodb://localhost:27017/resort_app")
-# db_path = f'postgresql://postgres:{postgrepass}@localhost:5432/SkiResorts'
-# engine = create_engine(db_path)
 
-# # Set routes
+# Routes
+
 @app.route("/")
 def home():
     return render_template("index.html")
