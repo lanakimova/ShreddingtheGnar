e498fbff799f03f2e60bfdfc185b3b3b2818edce
Svetlana Akimova <havroshik+git@gmail.com>
2021-07-23 18:00:20 -0700

    update data file

--- static/js/skiMap.js
+++ static/js/skiMap.js
@@ -9,23 +9,6 @@ let streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
         accessToken: API_KEY
       }).addTo(myMap);
 
-showAllResorts();
-
-function showAllResorts() {
-    fetch('/getAllResorts').then(function(resp) {
-        return resp.json();
-      }).then(function(data) {
-        data.forEach(resort => {
-          lat = resort.coordinates[0];
-          lng = resort.coordinates[1];
-      
-          let popupContent = "<b style='font-size: 16px'>" + resort.name + "</b><br><b>Total Slope Lenght:</b> " + resort.total_length
-                                           + "<br><b> Lift Price:</b> $" + resort.price + "<br><b> Closest Town:</b> " + resort.closest_town;
-          L.marker([lng, lat]).bindPopup(popupContent).addTo(myMap); 
-        })
-      })
-};
-
 // Create a drop down menu with states
 const dropDownMenu = L.control({position: 'topright'});
 
@@ -38,7 +21,6 @@ dropDownMenu.onAdd = function () {
   let opt = document.createElement('option');
   opt.setAttribute('value', "All States");
   let firstNode = document.createTextNode("All States");
-  console.log(firstNode);
   opt.appendChild(firstNode);
   select.appendChild(opt);
 
@@ -64,126 +46,83 @@ dropDownMenu.onAdd = function () {
 
 dropDownMenu.addTo(myMap);
 
+// call func to viz resort on the map
+showAllResorts();
 
-// lisener for selected option
-d3.select('#statesDropDown').on('change', updateMap);
+//  event listener for swlected option
+document.getElementById('statesDropDown').addEventListener('change', updateMap);
+
+function showAllResorts() {
+    fetch('/getAllResorts').then(function(resp) {
+        return resp.json();
+      }).then(function(data) {
+        data.forEach(resort => {
+          let lat = resort.coordinates[0];
+          let lng = resort.coordinates[1];
+      
+          let popupContent = "<b style='font-size: 16px'>" + resort.name + "</b><br><b>Total Slope Lenght:</b> " + resort.total_length
+                                           + "<br><b> Lift Price:</b> $" + resort.price + "<br><b> Closest Town:</b> " + resort.closest_town;
+          L.marker([lng, lat]).bindPopup(popupContent).addTo(myMap); 
+        })
+      })
+};
 
 function updateMap(){
     // get value from dropdown
-    let dropDownStates = d3.select('#statesDropDown').node().value;
-    console.log(dropDownStates);
+    let sel = document.getElementById('statesDropDown');
+    let opt = sel.options[sel.selectedIndex];
+    let state = opt.text;
 
     //  remove previous layer with markers
     let stateMarkers = L.layerGroup();
     myMap.eachLayer(function(layer) { if (layer != streetMap) { myMap.removeLayer(layer) }});
 
-    if (dropDownStates != 'All States') {
+    // Lana continue fron here 
+    if (state != 'All States') {
+
+        // hightlite given state and recenter the map
         fetch('/static/data/us_states.json').then(response => {
             return response.json();
         }).then(data => {
-            let features = data.features;
+                let features = data.features;
+            
+                features.forEach(feature => {
+                    if (feature.properties.name === state){
+                        let statePoly = L.geoJSON(feature).addTo(myMap);
+                        let poly = [];
+                        if (feature.geometry.type === 'Polygon') { poly = feature.geometry.coordinates[0]; };
         
-            features.forEach(feature => {
-                if (feature.properties.name === dropDownStates){
-                    let statePoly = L.geoJSON(feature).addTo(myMap);
-                    let poly = [];
-                    if (feature.geometry.type === 'Polygon') {
-                        poly = feature.geometry.coordinates[0];
-                    };
-    
-                    if (feature.geometry.type === 'MultiPolygon'){
-                        poly = feature.geometry.coordinates[0][0]
-                    };
-                    myMap.fitBounds(statePoly.getBounds());
-                    
-                    d3.csv("/static/data/complete_resorts_info.csv").then(function(data) {
-                        let markers = [];
-                        let resortNames = [],
-                            liftPrice = [],
-                            slopesLen = [];
-    
-                        data.forEach(resort => {
-                            if (resort.lon) {
-                                let lon = Number(resort.lon),
-                                    lat = Number(resort.lat);
-                                let marker = L.marker([lon, lat]).addEventListener('click', markerOnClick);
-                                if (isMarkerInsidePolygon(marker, poly)) {
-    
-                                    let rName = resort.name,
-                                        totalSlopeLen = resort.total_len,
-                                        closestTown = resort.closest_town,
-                                        liftTicket = resort.price;
-    
-                                    if (liftTicket) {
-                                        resortNames.push(rName);
-                                        liftPrice.push(liftTicket);
-                                        slopesLen.push(totalSlopeLen);
-                                    }
-    
-                                    let popupContent = "<b style='font-size: 16px'>" + rName + "</b><br><b>Total Slope Lenght:</b> " + totalSlopeLen
-                                                        + "<br><b> Lift Price:</b> $" + liftTicket + "<br><b> Closest Town:</b> " + closestTown;
-                                    
-                                    markers.push(marker.bindPopup(popupContent));
-                                    stateMarkers = L.layerGroup(markers).addTo(myMap);                           
-                                };
-                            };
-                        });
-    
-                        if (markers.length > 0) {
-                            if (document.getElementById('priceChart')) {
-                                document.getElementById('priceChart').remove();
-                            };
-    
-                            if (document.getElementById('slopeChart')) {
-                                document.getElementById('slopeChart').remove();
-                            };
-                            document.getElementById('price').insertAdjacentHTML('afterend', 
-                                        `<div id='priceChart'></div>`);
-                            
-                            document.getElementById('slope').insertAdjacentHTML('afterend',
-                                        `<div id='slopeChart'></div>`);
-    
-                            comparePrice(resortNames, liftPrice);
-                            compareSlopesLen(resortNames, slopesLen);
-                        }
-                        else {
-                            if (document.getElementById('priceChart')) {
-                                document.getElementById('priceChart').remove();
-                            };
-    
-                            if (document.getElementById('slopeChart')) {
-                                document.getElementById('slopeChart').remove();
-                            };                           
-                        };                       
-                    });
-                }; 
-            });
-        }); 
+                        if (feature.geometry.type === 'MultiPolygon'){ poly = feature.geometry.coordinates[0][0]};
+
+                        myMap.fitBounds(statePoly.getBounds());
+                    }
+                })
+            })
+        // add resorts markers to the map on given state
+        fetch(`getStateResorts/${state}`).then(resp => {
+            return resp.json();
+        }).then(function(data) {
+            data.forEach(resort => {
+                let lat = resort.coordinates[0];
+                let lng = resort.coordinates[1];
+      
+                let popupContent = "<b style='font-size: 16px'>" + resort.name + "</b><br><b>Total Slope Lenght:</b> " + resort.total_length
+                                           + "<br><b> Lift Price:</b> $" + resort.price + "<br><b> Closest Town:</b> " + resort.closest_town;
+                L.marker([lng, lat]).bindPopup(popupContent).addTo(myMap);
+            })
+        });
+        
     }
     else { 
         showAllResorts();
         myMap.setView([39.8283, -98.5795], 4);
         document.getElementById('priceChart').remove();
         document.getElementById('slopeChart').remove();
-    };
-      
-};
-
-function isMarkerInsidePolygon(marker, poly) {
-    var inside = false;
-
-    var x = marker.getLatLng().lat, y = marker.getLatLng().lng;
-    console.log("isMARKER FUNC", x, y );
-
-    for (let i=0; i < poly.length-1; i++){
-        let xi = poly[i][1], yi = poly[i][0];
-        let xj = poly[i+1][1], yj = poly[i+1][0];
-        var intersect = ((yi > y) != (yj > y))
-                && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-            if (intersect) inside = !inside;
     }
-    return inside;
-};
+      
+}
+
+
 
 function comparePrice(resortNames, liftPrice) {
     let trace = {
